fe83b2d509aa820f704abd6acb2248c7e797aff1
eflotta <eflotta@users.noreply.github.com>
2014-03-26 13:11:03 +0100

    create hu.js

---
+++ hu.js
@@ -0,0 +1,29 @@
+var TRANSLATIONS = {
+	"login" : "Belépés",
+	"Login error" : "Sikertelen belépés",
+	"initialize sdk" : "",
+	"Hello" : "",
+
+	"Today": "Ma",
+	"Yesterday": "Tegnap",
+	"Week": "Hét",
+	"Month": "Hónap",
+	"Custom": "Időszak",
+	"Previous": "vissza",
+	"Next": "előre",
+
+	"No Data Available.": "Nincs adat.",
+	"Add widget": "Hozzáadás",
+	"Cancel": "Mégse",
+	"Unit": "Járművek",
+	"Unit group": "Járműcsoportok",
+
+	"Show units:": "Vizsgált járművek",
+	"Skip zeroth values": "Nulla értékek kihagyása",
+	"N/A": "",
+
+	'Oh! Unfortunately, you can\'t login': 'Hibás bejelentkezés.',
+	'Oh! %%module%% didn\'t load!': 'Error loading the app.',
+	'Oh! Unfortunately, we can\'t load SDK': 'Error loading the app.',
+	'Session has been lost': 'Session lost.'
+};
